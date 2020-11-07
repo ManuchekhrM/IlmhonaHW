076d3b242bae059fffef4cdde20715b44dc19418
Melibaev Manuchekhr <melibaev.m.i@gmail.com>
2020-11-07 20:06:30 +0500

    Dobavil massiv, no na vivode otveta ostanovilsya

--- Lesson_13/Zadacha_1/src/com/company/zadacha_1/Main.java
+++ Lesson_13/Zadacha_1/src/com/company/zadacha_1/Main.java
@@ -6,10 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 	    Pokupatel pokupatel = new Pokupatel("Odilov", "Rustam", "Saidovich",
-                "Behzod 8", 22223333, 44);
+                "Behzod 8", 22223333, 46);
 
-        System.out.println(pokupatel.getIdNumber());
-        pokupatel.getName();
+        Pokupatel pokupatel1 = new Pokupatel("Bekov", "Anush", "Bekovich",
+                "Behzod 6", 22224444, 55);
+
+        Pokupatel pokupatel2 = new Pokupatel("Khalilov", "Kholiq", "Beko",
+                "Behzod 7", 22221444, 27);
+
+        Pokupatel pokupatel3 = new Pokupatel("Win", "Dows", "Bek",
+                "Behzod 5", 223224444, 13);
+
+        Pokupatel pokupatel4 = new Pokupatel("Holov", "AShamil", "Ich",
+                "Behzod 4", 22224344, 29);
+
+
+        Pokupatel[] pokupatels = new Pokupatel[5];
+        pokupatels[0] = pokupatel;
+        pokupatels[1] = pokupatel1;
+        pokupatels[2] = pokupatel2;
+        pokupatels[3] = pokupatel3;
+        pokupatels[4] = pokupatel4;
+        int chetnoe = 0;
+            for(int i = 0; i <= 4; i++ ){
+                if(pokupatels[i].getIdNumber() % 2 == 0)
+                    chetnoe = pokupatels[i].getIdNumber();
+            System.out.println(chetnoe);
+    }
 
 
 
--- Lesson_13/Zadacha_2/src/com/company/zadacha_2/Main.java
+++ Lesson_13/Zadacha_2/src/com/company/zadacha_2/Main.java
@@ -6,9 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         Abonent abonent = new Abonent(66, "Anush", "Bekov",
-                "Bekovich", "Zarafshon 11", 55, 33);
+                "Bekovich", "Zarafshon 11", 53, 33);
 
-        System.out.println(abonent.getTimeCityCall());
-        abonent.getName();
+        Abonent abonent1 = new Abonent(66, "NOzanin", "Bekova",
+                "Bekovich", "Zarafshon 15", 54, 35);
+
+        Abonent abonent2 = new Abonent(66, "Anusha", "Beki",
+                "Bekovich", "Zarafshon 14", 55, 36);
+
+        Abonent abonent3 = new Abonent(66, "Anvara", "Bekovi",
+                "Bekovich", "Zarafshon 13", 56, 38);
+
+        Abonent abonent4 = new Abonent(66, "Azarat", "Belova",
+                "Bekovich", "Zarafshon 12", 57, 34);
+
+        Abonent[] abonents = new Abonent[5];
+        abonents[0] = abonent;
+        abonents[1] = abonent1;
+        abonents[2] = abonent2;
+        abonents[3] = abonent3;
+        abonents[4] = abonent4;
+        int peregovori = 0;
+        for(int i = 0; i <= 4; i++ ){
+            if(abonents[i].getTimeIntercityCall() > peregovori) {
+                peregovori = abonents[i].getTimeIntercityCall();
+
+            }
+        System.out.println(peregovori);
+
     }
-}
+}}
